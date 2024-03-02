d0d0aa2fa391019df01362de1c8726fbb1b78236
Nachappa <Nachappapr1991@gmail.com>
2024-03-02 09:25:42 +0530

    chore: update status card text color

--- app/components/StatusCard.tsx
+++ app/components/StatusCard.tsx
@@ -8,7 +8,8 @@ const StatusCard = ({ status }: { status: "draft" | "pending" | "paid" }) => {
         `px-2 py-2 flex justify-around items-center bg-opacity-5 rounded-md min-w-[100px] max-w-[100px] capitalize`,
         {
           "bg-green-1000 bg-opacity-5 !text-green-1000": status === "paid",
-          "bg-gray-1050 bg-opacity-5 !text-gray-1050": status === "draft",
+          "bg-gray-1050 bg-opacity-5 !text-gray-1050 dark:!text-indigo-1000":
+            status === "draft",
           "bg-orange-1000 bg-opacity-5 !text-orange-1000": status === "pending",
         }
       )}
